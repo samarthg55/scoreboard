a8e98b5347426fd2152aa08ba0ca475daa28cd89
samarthg55 <53981261+samarthg55@users.noreply.github.com>
2022-12-11 19:38:33 +0530

    inital creation

---
+++ index.js
@@ -0,0 +1,43 @@
+let hscore = document.getElementById("hid")
+let gscore = document.getElementById("gid")
+let hfinal = 0
+let gfinal = 0
+
+function hadd1()
+{
+    hfinal += 1
+    hscore.textContent = hfinal
+}
+
+function hadd2()
+{
+    hfinal += 2
+    hscore.textContent = hfinal
+}
+
+function hadd3()
+{
+    hfinal += 3
+    hscore.textContent = hfinal
+}
+
+
+
+
+function gadd1()
+{
+    gfinal += 1
+    gscore.textContent = gfinal
+}
+
+function gadd2()
+{
+    gfinal += 2
+    gscore.textContent = gfinal
+}
+
+function gadd3()
+{
+    gfinal += 3
+    gscore.textContent = gfinal
+}
